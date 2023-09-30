add102eacc6c5a5e20f5a833eaa6090d28f762d2
Ambar Pathak <ambarpathak@gwu.edu>
2023-09-30 12:39:38 -0400

    new addition in parallel processing func.

--- twitter_in_house.py
+++ twitter_in_house.py
@@ -1,25 +1,31 @@
+#%%
+from  selenium import webdriver
 from twitter_scraper_selenium import scrape_keyword
 import json
 import pandas as pd
 import asyncio
 
-def scrape_profile_tweets_since_2023(username: str):
-    kword = "from:" + username
-    path = './users/' + username
-    file_path = path + '.csv'
-    tweets = scrape_keyword(
-                            headless=True,
-                            keyword=kword,
-                            browser="chrome",
-                            tweets_count=2, # Just last 2 tweets
-                            filename=path,
-                            output_format="csv",
-                            since="2023-01-01",
-                            # until="2025-03-02", # Until Right now
-                            )
-    data = pd.read_csv(file_path)
-    data = json.loads(data.to_json(orient='records'))
-    return data
+#%%
+
+def scrape_profile_tweets_since_2023(username: str, driver: webdriver.Chrome):
+  kword = "from:" + username
+  path = 'C:/Users/patha/OneDrive/Desktop/MS in USA/DSSD Outreach' + username
+  file_path = path + '.csv'
+  tweets = scrape_keyword(
+      headless=True,
+      keyword=kword,
+      browser="chrome",
+      tweets_count=2, # Just last 2 tweets
+      filename=path,
+      output_format="csv",
+      since="2023-01-01",
+      # until="2025-03-02", # Until Right now
+      driver=driver
+  )
+  data = pd.read_csv(file_path)
+  data = json.loads(data.to_json(orient='records'))
+  return data
+
 
 from multiprocessing import Pool
 
@@ -28,10 +34,17 @@ from multiprocessing import Pool
 
 # Run in parallely
 def functionToRunParallely(i):
-    return i
+    global driver
+    if driver is None:
+        driver = webdriver.Chrome()
+    data = scrape_profile_tweets_since_2023(username=i, driver=driver)
+    driver.close()
+    return data
 
 noOfPools = 5
 
 if __name__ == "__main__":
+    driver = webdriver.Chrome()
     with Pool(noOfPools) as p:
-        p.map(scrape_profile_tweets_since_2023,['elonmusk', 'BarackObama', 'cathiedwood'])
+        p.map(functionToRunParallely,['elonmusk', 'BarackObama', 'cathiedwood'])
+# %%
