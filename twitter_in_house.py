0df651df0f40b197805e244ec3f3abd98c2b822f
Udbhav Kush <ukush4@gwmail.gwu.edu>
2023-10-07 12:28:27 -0400

    basic scraper working

--- twitter_in_house.py
+++ twitter_in_house.py
@@ -1,37 +1,72 @@
-from twitter_scraper_selenium import scrape_keyword
-import json
-import pandas as pd
-import asyncio
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
-def scrape_profile_tweets_since_2023(username: str):
-    kword = "from:" + username
-    path = './users/' + username
-    file_path = path + '.csv'
-    tweets = scrape_keyword(
-                            headless=True,
-                            keyword=kword,
-                            browser="chrome",
-                            tweets_count=2, # Just last 2 tweets
-                            filename=path,
-                            output_format="csv",
-                            since="2023-01-01",
-                            # until="2025-03-02", # Until Right now
-                            )
-    data = pd.read_csv(file_path)
-    data = json.loads(data.to_json(orient='records'))
-    return data
 
-from multiprocessing import Pool
 
-# Just one account
-# scrape_profile_tweets_since_2023('elonmusk')
 
-# Run in parallely
-def functionToRunParallely(i):
-    return i
 
-noOfPools = 5
+l=list()
+o={}
 
-if __name__ == "__main__":
-    with Pool(noOfPools) as p:
-        p.map(scrape_profile_tweets_since_2023,['elonmusk', 'BarackObama', 'cathiedwood'])
+target_url = "https://twitter.com/elonmusk"
+
+
+driver=webdriver.Chrome()
+
+driver.get(target_url)
+time.sleep(2)
+
+
+
+resp = driver.page_source
+driver.close()
+
+soup=BeautifulSoup(resp,'html.parser')
+
+try:
+    o["profile_name"]=soup.find("div",{"class":"r-1vr29t4"}).text
+except:
+    o["profile_name"]=None
+
+try:
+    o["profile_handle"]=soup.find("div",{"class":"r-1wvb978"}).text
+except:
+    o["profile_handle"]=None
+
+try:
+    o["profile_bio"]=soup.find("div",{"data-testid":"UserDescription"}).text
+except:
+    o["profile_bio"]=None
+
+profile_header = soup.find("div",{"data-testid":"UserProfileHeader_Items"})
+
+try:
+    o["profile_category"]=profile_header.find("span",{"data-testid":"UserProfessionalCategory"}).text
+except:
+    o["profile_category"]=None
+
+try:
+    o["profile_website"]=profile_header.find('a').get('href')
+except:
+    o["profile_website"]=None
+
+try:
+    o["profile_joining_date"]=profile_header.find("span",{"data-testid":"UserJoinDate"}).text
+except:
+    o["profile_joining_date"]=None
+
+try:
+    o["profile_following"]=soup.find_all("a",{"class":"r-rjixqe"})[0].text
+except:
+    o["profile_following"]=None
+
+try:
+    o["profile_followers"]=soup.find_all("a",{"class":"r-rjixqe"})[1].text
+except:
+    o["profile_followers"]=None
+
+
+l.append(o)
+
+print(l)
